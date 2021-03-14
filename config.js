520e58c78688e023bd5dc1b152e6f9207e973ddc
Ramdhei <ramdhei2002@gmail.com>
2021-03-14 11:43:32 -0500

    Sent to prod with vercel secrets

--- config.js
+++ config.js
@@ -4,13 +4,13 @@ module.exports = {
         port: process.env.PORT || 3000
     },
     jwt: {
-        secret: process.env.JWT_SECRET|| '!notasecret'
+        secret: process.env.JWT_SECRET|| ''
     },
     mysql: {
-        host: process.env.MYSQL_HOST || 'remotemysql.com',
-        user: process.env.MYSQL_USER || 'huAj7Zwewd',
-        password: process.env.MYSQL_PASSWORD || 'irdYQbC4yc',
-        database: process.env.MYSQL_DB || 'huAj7Zwewd',
+        host: process.env.MYSQL_HOST || '',
+        user: process.env.MYSQL_USER || '',
+        password: process.env.MYSQL_PASSWORD || '',
+        database: process.env.MYSQL_DB || '',
     },
     post: {
         port: process.env.POST_PORT || 3002,
